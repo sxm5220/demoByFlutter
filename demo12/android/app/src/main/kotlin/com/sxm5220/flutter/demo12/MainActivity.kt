e300b5c5de7d44b477de13dfac6c0db4f59471c1
sxm5220 <sxm5220@gmail.com>
2021-06-21 18:45:36 +0800

    refine: 抖音demo

---
+++ demo12/android/app/src/main/kotlin/com/sxm5220/flutter/demo12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sxm5220.flutter.demo12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo12/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:demo12/style/style.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'homePage.dart';
+
+void main() {
+  //自定义报错页面
+  if (kReleaseMode) {
+    ErrorWidget.builder = (FlutterErrorDetails detail) {
+      debugPrint(detail.toString());
+      return Material(
+        child: Center(
+          child: Text(
+            '发生了没有处理的错误\n请通知开发者',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
+    };
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Tiktok',
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          hintColor: Colors.white,
+          accentColor: Colors.white,
+          primaryColor: ColorPlate.orange,
+          primaryColorBrightness: Brightness.dark,
+          scaffoldBackgroundColor: ColorPlate.back1,
+          dialogBackgroundColor: ColorPlate.back2,
+          accentColorBrightness: Brightness.light,
+          textTheme: TextTheme(bodyText1: StandardTextStyle.normal)),
+      home: HomePage(),
+    );
+  }
+}
